d873e6ce29ad284b11fb51eacf348f3354bf84ef
Marko Mijakovski <m.mijakovski07@gmail.com>
2025-12-08 15:26:24 +0100

    testing server online

--- server/index.js
+++ server/index.js
@@ -4,19 +4,19 @@ import multer from "multer";
 import cookieParser from "cookie-parser";
 
 //import from embedder.js
-import {Find_Closest_Match} from './components/embedder.js'
+// import {Find_Closest_Match} from './components/embedder.js'
 
 //import from userDB.js
-import { singUp, logIn, userDetails, userUpdates, DeleteUser} from './components/usersDB.js'
+// import { singUp, logIn, userDetails, userUpdates, DeleteUser} from './components/usersDB.js'
 
 // import from likesDB.js
-import {checkLiked, Like, likesCountTrack, everyLiked, DeleteLiked} from './components/likesDB.js'
+// import {checkLiked, Like, likesCountTrack, everyLiked, DeleteLiked} from './components/likesDB.js'
 
 //import from commentsDB.js
-import {addComent, addReply, addReaction, getComments, deleteComment} from './components/commentsDB.js'
+// import {addComent, addReply, addReaction, getComments, deleteComment} from './components/commentsDB.js'
 
 // import from token.js
-import {auth} from './components/token.js'
+// import {auth} from './components/token.js'
 const app = express();
 const PORT = 3000;
 
@@ -40,6 +40,17 @@ app.use(cors({
 // Middleware for JSON
 app.use(express.json());
 
+// root server
+app.get('/', (req, res) => {
+    res.send('Server is running ✅');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
+if(false ) {
 
 // AI search rout
 app.post('/myQuery', async(req, res) => {
@@ -346,3 +357,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+}
